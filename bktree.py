36ac47abe49f27b6aae1b3d731db3e189cb12433
AlmasZhorayev <almas.zhorayev156@gmail.com>
2026-07-14 13:58:40 -0400

    levenstein

--- bktree.py
+++ bktree.py
@@ -1,3 +1,5 @@
+import Levenshtein
+
 class BKTreeNode:
     def __init__(self, word):
         self.word = word
@@ -6,3 +8,17 @@ class BKTreeNode:
 class BKTree:
     def __init__(self):
         self.root = None
+
+    def add(self, word):
+        if self.root is None:
+            self.root = BKTreeNode(word)
+            return
+        current = self.root
+        while True:
+            dist = Levenshtein.distance(word, current.word)
+            if dist == 0:
+                return
+            if dist not in current.children:
+                current.children[dist] = BKTreeNode(word)
+                return
+            current = current.children[dist]
