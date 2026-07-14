b844909e96d15cc01c2ea16feacf1900ea91a165
AlmasZhorayev <almas.zhorayev156@gmail.com>
2026-07-14 13:35:50 -0400

    division hash

--- hashing.py
+++ hashing.py
@@ -4,7 +4,7 @@ def ascii_sum(string, table_size):
     total = 0
     for char in string:
         total + ord(char)
-    return total % table_size
+    return str(total % table_size)
 
 def jenkins_hash(string, table_size):
     hash_value = 0
@@ -16,3 +16,11 @@ def jenkins_hash(string, table_size):
     hash_value ^= hash_value >> 11
     hash_value += hash_value << 15
     return str(hash_value % table_size)
+
+def division(string, table_size, seed):
+    primes = [31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+    prime = primes[seed]
+    total = 0
+    for i in range(len(string)):
+        total += ord(string[i]) * (prime * i)
+    return str(total % table_size)
