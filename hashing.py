89471626eecccd7f25f66fdc593772b3af79817f
AlmasZhorayev <almas.zhorayev156@gmail.com>
2026-07-14 13:33:43 -0400

    jenkins hash

--- hashing.py
+++ hashing.py
@@ -5,3 +5,14 @@ def ascii_sum(string, table_size):
     for char in string:
         total + ord(char)
     return total % table_size
+
+def jenkins_hash(string, table_size):
+    hash_value = 0
+    for char in string:
+        hash_value += ord(char)
+        hash_value += hash_value << 10
+        hash_value ^= hash_value >> 8
+    hash_value += hash_value << 3
+    hash_value ^= hash_value >> 11
+    hash_value += hash_value << 15
+    return str(hash_value % table_size)
